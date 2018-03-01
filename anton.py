b35ab925b4ed9044ed55a5edb537e761b1f7f2b7
antongoransson <anton.goransson95@gmail.com>
2018-03-01 21:07:29 +0100

    Validator almost owrking

--- anton.py
+++ anton.py
@@ -2,54 +2,61 @@ from sys import argv
 from math import floor, sqrt
 from random import randint
 
-
-
 def dist(start, end):
     return abs(start[0] - end[0] + start[1] - end[1])
 
 def validate(rides, vehicles_rides, B, T):
     score = 0
+    # vehicles_rides = [[1, 0],[2, 2, 1]]
     for vehicle in vehicles_rides:
         vehicle_start = (0, 0)
-        temp = T
+        steps = 0
         for v in vehicle[1:]:
-            r_start = (rides[v][0], rides[v][1])
-            v_dist = dist(vehicle_start, r_start)
-            temp -= v_dist
-            if temp <= rides[v][4]:
+            v_dist = dist(vehicle_start, rides[v].from_pos)
+            steps += v_dist
+            if steps <= rides[v].start:
+                steps = rides[v].start
                 score += B
-            r_end = (rides[v][2], rides[v][3])
-            score += dist(r_start, r_end)
-            vehicle_start = r_end
-            # if vehicle_start[0] == rides[v][0] and vehicle_start[1] == rides[v][1]:
-            # vehicle_start[0] = rides[v][2]
-            # vehicle_start[1] = rides[v][3]
+            ride_dist = dist(rides[v].from_pos, rides[v].to_pos)
+            steps += ride_dist
+            vehicle_start = rides[v].to_pos
+            if steps > rides[v].finish: continue
+            score += ride_dist
 
     print(score)
-if __name__ == '__main__':
- fname = 'inputs/a_example.in'
+class Ride:
+  def __init__(self, index, a, b, x, y, s, f):
+    self.index = index
+    self.from_pos = (a, b)
+    self.to_pos = (x, y)
+    self.start = s
+    self.finish = f
 
- if len(argv) == 2:
-   fname = argv[1]
+if __name__ == "__main__":
+  fname = 'inputs/b_should_be_easy.in'
 
- with open(fname) as f:
-   R, C, F, N, B, T = map(int, f.readline().split(' '))
-   content = f.readlines()
+  if len(argv) == 2:
+    fname = argv[1]
 
-   rides1 = [list(map(int, x.strip().replace(" ", ""))) for x in content]
-   print(rides1)
- rides = list(range(N))
- vehicles = [[] for i in range(F)]
- vehicle_index = 0
+  with open(fname) as f:
+    R, C, F, N, B, T = map(int, f.readline().split(' '))
+    content = f.readlines()
+    # rides1 = [list(map(int, x.strip().replace(" ", ""))) for x in content]
+    rides = [ Ride(i, *list(map(int, x.strip().split(' ')))) for i, x in enumerate(content)]
+    val_rides = [r for r in rides]
+  vehicles = [[] for i in range(F)]
+  rides.sort(key=lambda x: x.start, reverse=False)
+  vehicle_index = 0
 
- while len(rides) > 0:
-   ride_index = randint(0, len(rides) - 1)
-   ride = rides.pop(ride_index)
-   vehicles[vehicle_index % F].append(ride)
-   vehicle_index += 1
+  while len(rides) > 0:
+    ride_index = 0 # randint(0, len(rides) - 1)
+    ride = rides.pop(ride_index)
+    vehicles[vehicle_index % F].append(ride.index)
+    vehicle_index += 1
 
- vehicles_rides = [ [len(vehicle), *vehicle] for vehicle in vehicles]
- validate(rides1, vehicles_rides, B, T)
- with open('outputs/solution.txt', 'w') as f:
-   for vehicle in vehicles_rides:
-     f.write(' '.join([str(ride) for ride in vehicle]) + '\n')
+  vehicles_rides = [ [len(vehicle), *vehicle] for vehicle in vehicles]
+  validate(val_rides, vehicles_rides, B, T)
+
+  with open('outputs/' + fname.split('/')[1], 'w') as f:
+    for vehicle in vehicles_rides:
+      f.write(' '.join([str(ride) for ride in vehicle]) + '\n')
