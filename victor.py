deccbea9577ac256565ffd4d0e74d06a5e543285
VictorWinberg <victor.m.winberg@gmail.com>
2018-03-01 19:54:27 +0100

    add bash script

--- victor.py
+++ victor.py
@@ -25,6 +25,6 @@ if __name__ == "__main__":
 
   vehicles_rides = [ [len(vehicle), *vehicle] for vehicle in vehicles]
 
-  with open('outputs/solution.txt', 'w') as f:
+  with open('outputs/' + fname.split('/')[1], 'w') as f:
     for vehicle in vehicles_rides:
       f.write(' '.join([str(ride) for ride in vehicle]) + '\n')
