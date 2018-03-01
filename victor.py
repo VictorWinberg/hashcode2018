196a3ec53e861bdc78f7bcb5245007ad78ec2de7
VictorWinberg <victor.m.winberg@gmail.com>
2018-03-01 19:31:04 +0100

    add rides and vechicles vectors

--- victor.py
+++ victor.py
@@ -9,6 +9,18 @@ if __name__ == "__main__":
     fname = argv[1]
 
   with open(fname) as f:
-    rows, columns, vehicles, rides, bonus, steps = map(int, f.readline().split(' '))
+    R, C, F, N, B, T = map(int, f.readline().split(' '))
     content = f.readlines()
     rides = [list(x.strip()) for x in content]
+
+  rides = list(range(N))
+  vehicles = [[]] * F
+  vehicle_index = 0
+
+  while len(rides) > 0:
+    ride_index = randint(0, len(rides) - 1)
+    ride = rides.pop(ride_index)
+    vehicles[vehicle_index % F].append(ride)
+    vehicle_index += 1
+
+  print(vehicles)
